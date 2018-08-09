b6426c9ec52b2034eb5d36a1e5eb92208e97e1cc
lililiaaa <1123543277@qq.com>
2018-08-09 14:29:17 +0800

    test

---
+++ vue/vue-cli/hello-world/src/components/ConnectCom1.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>com1</h1>
+    </div>
+</template>
---
+++ vue/vue-cli/hello-world/src/views/Connect.vue
@@ -0,0 +1,19 @@
+<template>
+    <div><h1>hhh</h1>
+     <router-link to="/connect/test1">test1</router-link>
+     <router-link to="/connect/test2">test2</router-link>
+     <div>
+        <router-view/>
+     </div>
+    </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+
+</style>
